44960903d4263086bbb0a2f055ad0070c6bcb506
abbytec <gpsmurfs@gmail.com>
2023-03-28 22:22:14 -0300

    5

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <meta itemProp="url" content="https://abbycv.pages.dev/"></meta>
+    <meta itemProp="url" content="https://abby-dev.ml"></meta>
     <meta itemProp="description" content="Soy programadora, me interesa trabajar en diseño UI/UX y aprender sobre deep learning y de Cloud Computing."></meta>
   </React.StrictMode>
 )
